c611e0c98a48e05c1c1f79374cbff056cb165880
caZeladaS <cazelada.s@gmail.com>
2023-03-07 16:22:25 -0300

    primer commit

---
+++ clases/4s/app.js
@@ -0,0 +1,70 @@
+//Dato del tipo number
+
+let edad = 33;
+console.log(edad);
+
+//Dato del tipo number con decimales
+
+let precio = 150.50;
+console.log(precio);
+
+//Dato del tipo string con comillas dobles
+
+let nombreUno = "Carlos";
+console.log(nombreUno);
+
+//Dato del tipo stringo con comillas simples
+
+let apellido = 'Zelada';
+console.log(apellido);
+
+//Dato booleano con valor true
+
+let verdadero = true;
+console.log(verdadero);
+
+//Dato booleano con valor false
+
+let falso = false;
+console.log(falso);
+
+//Dato del tipo Nan
+
+let notANumber = NaN;
+console.log(notANumber);
+
+//Dato del tipo null
+
+let nulo = null;
+console.log(nulo);
+
+//Dato undefined
+
+let sinValor="Sin valor";
+console.log(sinValor); 
+
+//Objeto literal
+
+let miComputadora = {
+    marca: 'Macbook Pro', 
+    modelo: 2017, 
+    estaEncendida: true
+};
+ 
+console.log(miComputadora);
+
+//Array
+
+let seriesFavoritas = ["breaking bad", "fiends"];
+console.log(seriesFavoritas);
+
+
+//Instalacion del prompt
+
+const prompt = require("prompt-sync")({sigint: true });
+
+let nombre = prompt("ingrese su nombre: ");
+console. log ("hola", nombre, "!!!");
+
+
+
