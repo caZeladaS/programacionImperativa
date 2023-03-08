f9301a27088b2a6ef4dccab435dcf068e05368a9
caZeladaS <cazelada.s@gmail.com>
2023-03-07 22:31:51 -0300

    primer commit

---
+++ clases/4s/opeMatematicos.js
@@ -0,0 +1,79 @@
+// Suma
+
+console.log(5 + 4);
+
+// Resta
+
+console.log(10 - 6);
+
+// Mulltiplicación
+
+console.log(3 * 4);
+
+// División
+
+console.log(10 / 2);
+
+//Módulo
+
+console.log(10 % 2);
+
+// Incremento
+
+let numero = 6;
+console.log( ++numero );
+
+// Decremento
+
+let otroNumero = 10;
+console.log( --otroNumero );
+
+// Comparación simple
+
+console.log( 5 == '5');
+
+// Comparación estricta
+
+console.log( 5 === '5');
+
+// Desigualdad simple
+
+console.log( 5 != '5');
+
+//Desigualdad estricta
+
+console.log( 5 !== '5');
+
+// Mayor que
+
+console.log( 10 > 6);
+
+// Mayor o igual que
+
+console.log( 6 >= 6 );
+
+// Menor 
+
+console.log( 7 < 7 );
+
+// Menor o igual que 
+
+console.log(  7 <= 7 );
+
+// Operadores Logicos
+
+// Operador &&
+
+console.log( false && true ); // false
+
+console.log( true && true ); // true
+
+// OR 
+
+console.log( false || false);
+
+// Concatenacion
+
+let nombre = 'Carlos';
+let apellido = 'Zelada';
+    console.log( nombre, apellido );
